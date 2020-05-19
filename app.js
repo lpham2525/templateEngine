600039ab91f59aab5bb63de5308870011b2b0e20
LanChi Pham <lpham2525@gmail.com>
2020-05-18 21:38:11 -0700

    made changes to functions and added variables

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const { writeFile, appendFile } = require('fs')
 const { promisify } = require('util')
 const writeFileSync = promisify(writeFile)
 const appendFileSync = promisify(appendFile)
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+let teammate = ''
 let responses = ''
-let done = false
+let role = ''
+let team = []
 
 const top = `
 <!DOCTYPE html>
@@ -14,64 +19,213 @@ const top = `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Engineering Team</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>`
+  <style>
+  body{
+    text-align: center;
+  }
 
-const employee = responses => {
-  return `
-  <div class="card bg-info mb-3" style="width:18rem;">
-    <div class="card-body">
-      <h5 class="card-title text-white bg-info mb-3">${responses.name}
-      <br>Role: ${responses.role}</h5>
+  .bar{
+    text-align: center;
+    font-size: 80px;
+    background-color: salmon;
+    color: white;
+    width: 100%;
+    height: 100px;
+    }
+
+  .info {
+      margin: 50px;
+      display: inline-block;
+    }
+  </style>
+</head>
+<body>
+<div class="bar">Team Roster</div>
+`
+
+function start() {
+  console.log("Welcome to the Template Engine!")
+  prompt([
+    {
+      type: "list",
+      name: "role",
+      message: "Which role do you want to input?",
+      choices: ["manager", "engineer", "intern", "None. I'm done."]
+    }
+  ])
+    .then(response => {
+      let roleQuestion
+      let questionList
+      switch (response.role) {
+        case "manager":
+          roleQuestion = getOffice()
+          questionList = questions(roleQuestion)
+          askQuestions(questionList, createManager())
+          break
+        case "engineer":
+          roleQuestion = getGithub()
+          questionList = questions(roleQuestion)
+          askQuestions(questionList, createEngineer)
+          break
+        case "intern":
+          roleQuestion = getSchool()
+          questionList = questions(roleQuestion)
+          askQuestions(questionList, createIntern)
+          break
+        case "None. I'm done.":
+          appendFileSync('team.html',
+            `</body>
+                  </html >`)
+        default:
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+function createTeam() {
+  writeFileSync('./output/team.html', top)
+    .then(responses => {
+      console.log(responses)
+      appendFileSync('./output/team.html', responses)
+    })
+    .catch(err => console.log(err))
+}
+
+function createManager() {
+  let teammate = new Manager(responses.name, responses.role, responses.id, responses.email, responses.office)
+  responses = `
+  <div class="info">
+    <div class="card bg-info mb-3" style="width:18rem;">
+      <div class="card-body">      
+        <h5 class="card-title text-white bg-info mb-3">${responses.name}
+        <br>Role: ${responses.role}</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${responses.id}</li>
+            <li class="list-group-item">Email: ${responses.email}</li>
+            <li class="list-group-item">Office Number: ${responses.office}</li>
+          </ul>
+      </div>
+    </div>
+  </div>`
+  team.push(teammate)
+  createTeam()
+  start()
+}
+
+function createEngineer() {
+  let teammate = new Engineer(responses.name, responses.role, responses.id, responses.title, responses.email, response.github)
+  responses = `
+  <div class="info">
+    <div class="card bg-info mb-3" style="width:18rem;">
+      <div class="card-body">      
+        <h5 class="card-title text-white bg-info mb-3">${responses.name}
+        <br>Role: ${responses.role}</h5>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${responses.id}</li>
             <li class="list-group-item">Title: ${responses.title}</li>
             <li class="list-group-item">Email: ${responses.email}</li>
+            <li class="list-group-item">Github: ${responses.github}</li>
           </ul>
-   </div>
-  </div>
-  `} //#where the dynamic HTML page ends
+      </div>
+    </div>
+  </div>`
+  team.push(teammate)
+  createTeam()
+  start()
+}
 
-writeFileSync('team.html', top)
-  .then(() => {
-    prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your id?"
-      },
-      {
-        type: "input",
-        name: "title",
-        message: "What is your title in the company?"
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
-      },
-      {
-        type: "list",
-        name: "role",
-        message: "What is your role in the company?",
-        choices: ["manager", "engineer", "intern"]
-      }
-    ])
 
-      .then(responses => {
-        console.log(responses)
-        done = true
-        appendFileSync('team.html', employee(responses))
-        if (employee(responses) && done === true) {
-          appendFileSync('team.html', `</body>
-                </html >`)//#where the dynamic HTML page begins again and inserts  the ending HTML tags
+function createIntern(responses) {
+  let teammate = new Intern(responses.name, responses.role, responses.id, responses.email, response.school)
+  responses = `
+  <div class="info">
+    <div class="card bg-info mb-3" style="width:18rem;">
+      <div class="card-body">      
+        <h5 class="card-title text-white bg-info mb-3">${responses.name}
+        <br>Role: ${responses.role}</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${responses.id}</li>
+            <li class="list-group-item">Email: ${responses.email}</li>
+            <li class="list-group-item">School: ${responses.role}</li>
+          </ul>
+      </div>
+    </div>
+  </div>`
+  team.push(teammate)
+  createTeam()
+  start()
+}
+
+function askQuestions(questionList, roleCreate) {
+  prompt(questionList)
+}
+
+function questions(roleQuestion) {
+  const questions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the teammate's name?",
+      validate: answer => {
+        if (!answer == '') {
+          return true
+        } else {
+          return "Please type in something for name."
         }
-      })
-      .catch(err => console.log(err))
-  })
-  .catch(err => console.log(err))
+      }
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the teammate's id?",
+      validate: answer => {
+        if (!answer == '') {
+          return true
+        } else {
+          return 'Please type in something for id.'
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the teammate's email?",
+      validate: answer => {
+        if (!answer == '') {
+          return true
+        } else {
+          return 'Please type in something for email.'
+        }
+      }
+    }
+  ]
+  questions.push(roleQuestion)
+  return questions
+}
+
+function getOffice() {
+  return {
+    type: "input",
+    name: "office",
+    message: "If manager has an office, what is the office number?"
+  }
+}
+
+function getGithub() {
+  return {
+    type: "input",
+    name: "github",
+    message: "If engineer has a Github account, what is the engineer's Github username?"
+  }
+}
+
+function getSchool() {
+  return {
+    type: "input",
+    name: "school",
+    message: "If the intern is in school, which school does the intern attend?"
+  }
+}
+
+start()
